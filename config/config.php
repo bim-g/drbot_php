170eab2e2ef2ca683e57d83b46da0e56c69f9646
bim-g <ibmussafb@gmail.com>
2019-04-21 11:21:34 +0200

    -ccreate folder config for configurations -create config file for configurations about the application and they global -create connect file for database connection.

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+    /**
+     * allow configuration are store in this file
+     * for database and and the connection 
+     * they declare as global tho to have full acces where ever in the project
+     */
+
+    define("HOST","localhost");
+    define("DATABASE","sentinel");
+    define("USER","root");
+    define("PASSWORD","");
